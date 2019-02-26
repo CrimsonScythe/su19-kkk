7f582d89d5dff750c79a0104de9570515c7ed53d
Crimson Scythe <crimsonscythestudios@gmail.com>
2019-02-26 15:28:18 +0100

    fixed namespace

--- SU19-Exercises/Galaga-Exercise-1/Game.cs
+++ SU19-Exercises/Galaga-Exercise-1/Game.cs
@@ -1,8 +1,11 @@
-
+using DIKUArcade;
+using DIKUArcade.EventBus;
 
 namespace Galaga_Exercise_1 {
     
-    public class Game {
-        
+    public class Game : IGameEventProcessor<object> {
+        public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
+            throw new System.NotImplementedException();
+        }
     }
 }
