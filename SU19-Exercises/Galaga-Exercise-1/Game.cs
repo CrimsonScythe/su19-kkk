1c400ec1850c3d4767a6137cb76466755d873965
Crimson Scythe <crimsonscythestudios@gmail.com>
2019-02-27 11:59:42 +0100

    added player logic

--- SU19-Exercises/Galaga-Exercise-1/Game.cs
+++ SU19-Exercises/Galaga-Exercise-1/Game.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using DIKUArcade;
 using DIKUArcade.Entities;
@@ -13,17 +14,27 @@ namespace Galaga_Exercise_1 {
 
         private Window win;
         private GameTimer gameTimer;
-
+        private GameEventBus<object> eventBus;
         private Player player;
 
         public Game() {
-            
-            player = new Player(this,
-                new DynamicShape(new Vec2F(0.45f, 0.1f),new Vec2F(0.1f, 0.1f) ),
-                new Image(Path.Combine("Assets", "Images", "Player.png")));
-            
+                
             win = new Window("test" ,500, 500);
             gameTimer = new GameTimer(60,60);
+            
+            player = new Player(this,
+                            new DynamicShape(new Vec2F(0.45f, 0.1f),new Vec2F(0.1f, 0.1f) ),
+                            new Image(Path.Combine("Assets", "Images", "Player.png")));
+            
+            eventBus = new GameEventBus<object>();
+            eventBus.InitializeEventBus(new List<GameEventType>() {
+                GameEventType.InputEvent,
+                GameEventType.WindowEvent,
+            });
+            win.RegisterEventBus(eventBus);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
+            eventBus.Subscribe(GameEventType.WindowEvent, this);
+
         }
 
         public void GameLoop() {
@@ -31,11 +42,19 @@ namespace Galaga_Exercise_1 {
                 gameTimer.MeasureTime();
                 while (gameTimer.ShouldUpdate()) {
                     win.PollEvents();
+                    
+                    player.Move();
+                    
+                    eventBus.ProcessEvents();
                     //update game logic here
                 }
 
                 if (gameTimer.ShouldRender()) {
                     win.Clear();
+                    
+                    player.RenderEntity();
+
+                    
                     //render gameplay entites here
                     win.SwapBuffers();
                 }
@@ -44,21 +63,57 @@ namespace Galaga_Exercise_1 {
                     win.Title = "Galaga | UPS: " + gameTimer.CapturedUpdates +
                                 ", FPS: " + gameTimer.CapturedFrames;
                 }
-//                player.RenderEntity();
             }
         }
 
         public void KeyPress(string key) {
-            throw new NotImplementedException();
+            switch (key) {
+                case "KEY_ESCAPE":
+                    eventBus.RegisterEvent(
+                        GameEventFactory<object>.CreateGameEventForAllProcessors(
+                            GameEventType.WindowEvent, this, "CLOSE_WINDOW", "", ""));
+                    break;
+                case "KEY_A":
+                    player.Direction(new Vec2F(-0.01f, 0.0f));    
+                    break;
+                
+                case "KEY_D":
+                    player.Direction(new Vec2F(0.01f, 0.0f));
+                    break;
+            }
         }
 
         public void KeyRelease(string key) {
-            throw new NotImplementedException();
+            switch (key) {
+                case "KEY_A":
+                    player.Direction(new Vec2F(0.0f, 0.0f));
+                    break;
+                case "KEY_D":
+                    player.Direction(new Vec2F(0.0f, 0.0f));
+                    break;
+            }
         }
         
 
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
-            throw new NotImplementedException();
+            if (eventType == GameEventType.WindowEvent) {
+                switch (gameEvent.Message) {
+                    case "CLOSE_WINDOW":
+                        win.CloseWindow();
+                        break;
+                    default:
+                        break;
+                }
+            } else if (eventType == GameEventType.InputEvent) {
+                switch (gameEvent.Parameter1) {
+                    case "KEY_PRESS":
+                        KeyPress(gameEvent.Message);
+                        break;
+                    case "KEY_RELEASE":
+                        KeyRelease(gameEvent.Message);
+                        break;
+                }
+            }
         }
     }
 }
--- SU19-Exercises/Galaga-Exercise-1/Player.cs
+++ SU19-Exercises/Galaga-Exercise-1/Player.cs
@@ -1,14 +1,36 @@
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga_Exercise_1 {
     public class Player : Entity {
 
         private Game game;
+        private Shape shape;
         
         
-        public Player(Game game, DynamicShape shape, IBaseImage image) : base(shape, image) {
+        public Player(Game game, Shape shape, IBaseImage image) : base(shape, image) {
             this.game = game;
+            this.shape = shape;
         }
+
+        public void Direction(Vec2F vec2F) {
+            //code below makes it more generic?
+            this.shape.AsDynamicShape().ChangeDirection(vec2F);
+            // OR
+//            this.shape.ChangeDirection(vec2F);
+        }
+
+        public void Move() {
+            //moving right
+            if (shape.AsDynamicShape().Direction.X > 0.0f && shape.Position.X < 0.90f) {
+                shape.Move();
+            }
+            //moving left
+            if (shape.AsDynamicShape().Direction.X < 0.0f && shape.Position.X > 0) {
+                shape.Move();
+            }
+        }
+        
     }
 }
