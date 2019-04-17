759ab9d2fd88ca75eaac74abfdf9d316c738063b
Crimson Scythe <crimsonscythestudios@gmail.com>
2019-04-17 12:07:16 +0200

    fixed file path error

--- SU19-Exercises/SpaceTaxi-1/ASCII Loader.cs
+++ SU19-Exercises/SpaceTaxi-1/ASCII Loader.cs
@@ -6,20 +6,40 @@ namespace SpaceTaxi_1 {
         
         private string fileName;
         private string fileLoaded;
-        private string defaultPath = @"C:\su19-kkk\SU19-Exercises\SpaceTaxi-1\Levels\";
-        private string legendHolder;
+//        private string defaultPath = @"C:\su19-kkk\SU19-Exercises\SpaceTaxi-1\Levels\";
+//        private  legendHolder;
         
         public AsciiLoader(string fileName) {
-            this.fileName = defaultPath + fileName;
-            
+            this.fileName = fileName;
+
         }
 
         public void ReadText() {
-            fileLoaded = File.ReadAllText(this.fileName);
+            fileLoaded = File.ReadAllText(GetLevelFilePath(fileName));
 
-            
+//            Console.WriteLine(Path.GetDirectoryName());
 //            Console.WriteLine("df");
-//            Console.WriteLine(fileLoaded);
+            Console.WriteLine(fileLoaded);
+        }
+        
+        private string GetLevelFilePath(string filename) {
+            // Find base path.
+            DirectoryInfo dir = new DirectoryInfo(Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().Location));
+
+            while (dir.Name != "bin") {
+                dir = dir.Parent;
+            }
+            dir = dir.Parent;
+
+            // Find level file.
+            string path = Path.Combine(dir.FullName.ToString(), "Levels", filename);
+
+            if (!File.Exists(path)) {
+                throw new FileNotFoundException($"Error: The file \"{path}\" does not exist.");
+            }
+
+            return path;
         }
         
     }
