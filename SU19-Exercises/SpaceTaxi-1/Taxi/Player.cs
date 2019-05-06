9ea980c28b6ec81b246c3b3d3e33510604bcf089
spz377 <48015574+spz377@users.noreply.github.com>
2019-05-06 14:36:06 +0200

    Add files via upload
    Added thrust movements in ProcessEvent

--- SU19-Exercises/SpaceTaxi-1/Taxi/Player.cs
+++ SU19-Exercises/SpaceTaxi-1/Taxi/Player.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.EventBus;
 using DIKUArcade.Graphics;
@@ -11,6 +10,7 @@ namespace SpaceTaxi_1 {
         private readonly Image taxiBoosterOffImageRight;
         private readonly DynamicShape shape;
         private Orientation taxiOrientation;
+        public Vec2F thrust = new Vec2F(0f, 0f);
 
         public Player() {
             shape = new DynamicShape(new Vec2F(), new Vec2F());
@@ -45,12 +45,26 @@ namespace SpaceTaxi_1 {
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
             if (eventType == GameEventType.PlayerEvent) {
                 switch (gameEvent.Message) {
-                    case "BOOSTER_UPWARDS" :
-                        Console.WriteLine("up");
-                        break;
-                    case "STOP_ACCELERATE_UP" :
-                        Console.WriteLine("stop up");
-                        break;
+                case "BOOSTER_UPWARDS":
+                    thrust.Y = 0.000005f;
+                    break;
+                case "STOP_ACCELERATE_UP":
+                    thrust.Y = 0f;
+                    break;
+                case "BOOSTER_TO_LEFT":
+                    thrust.X = -0.000005f;
+                    break;
+                case "STOP_ACCELERATE_LEFT":
+                    thrust.X = 0f;
+                    break;
+                case "BOOSTER_TO_RIGHT":
+                    thrust.X = 0.000005f;
+                    break;
+                case "STOP_ACCELERATE_RIGHT":
+                    thrust.X = 0f;
+                    break;
+                    
+                // in the future, we will be handling movement here
                 }
             }
         }
