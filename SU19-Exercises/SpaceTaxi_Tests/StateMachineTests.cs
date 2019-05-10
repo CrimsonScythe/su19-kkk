976235e6894e05195b3cac7ff08e65644355140b
nikolaiknielsen <47968541+nikolaiknielsen@users.noreply.github.com>
2019-05-10 10:08:58 +0200

    Add files via upload

---
+++ SU19-Exercises/SpaceTaxi_Tests/StateMachineTests.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using DIKUArcade.EventBus;
+using NUnit.Framework;
+using SpaceTaxi_1;
+
+namespace SpaceTaxi_Test
+{
+    public class StateMachineTests {
+        private StateMachine stateMachine;
+
+        [SetUp]
+        public void InitiateStateMachine() {
+            
+            SpaceTaxiBus.GetBus().InitializeEventBus(new List<GameEventType>() {
+                GameEventType.InputEvent,
+                GameEventType.WindowEvent, 
+                GameEventType.GameStateEvent,
+                GameEventType.PlayerEvent
+            });
+            
+            
+            Game game = new Game();
+            stateMachine = new StateMachine(game);
+
+        }
+
+        [Test]
+        public void TestInitialState() {
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<MainMenu>());
+        }
+
+        [Test]
+        public void TestEventGamePaused() {
+            
+            SpaceTaxiBus.GetBus().RegisterEvent(GameEventFactory<object>.CreateGameEventForAllProcessors(
+                GameEventType.GameStateEvent,
+                this,
+                "CHANGE_STATE",
+                "GAME_PAUSED",""));
+            
+            SpaceTaxiBus.GetBus().ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<GamePaused>());
+
+        }
+
+        [Test]
+        public void TestEventGameRunning() {
+            SpaceTaxiBus.GetBus().RegisterEvent(GameEventFactory<object>.CreateGameEventForAllProcessors(
+                GameEventType.GameStateEvent,
+                this,
+                "CHANGE_STATE",
+                "GAME_RUNNING",""));
+            
+            SpaceTaxiBus.GetBus().ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<GameRunning>());
+        }
+        
+        [Test]
+        public void TestEventMainMenu() {
+            SpaceTaxiBus.GetBus().RegisterEvent(GameEventFactory<object>.CreateGameEventForAllProcessors(
+                GameEventType.GameStateEvent,
+                this,
+                "CHANGE_STATE",
+                "MAIN_MENU",""));
+            
+            SpaceTaxiBus.GetBus().ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<MainMenu>());
+        }
+
+        [Test]
+        public void TestEventChooseLevel() {
+            SpaceTaxiBus.GetBus().RegisterEvent(GameEventFactory<object>.CreateGameEventForAllProcessors(
+                GameEventType.GameStateEvent,
+                this,
+                "CHANGE_STATE",
+                "CHOSE_LEVEL", ""));
+
+            SpaceTaxiBus.GetBus().ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<ChoseLevel>());
+        }
+
+        [Test]
+        public void TestEventGameOver()
+        {
+            SpaceTaxiBus.GetBus().RegisterEvent(GameEventFactory<object>.CreateGameEventForAllProcessors(
+                GameEventType.GameStateEvent,
+                this,
+                "CHANGE_STATE",
+                "GAME_OVER", ""));
+
+            SpaceTaxiBus.GetBus().ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<GameOver>());
+        }
+    }
+}
+  
