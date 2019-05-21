1d5f90c3c35e2b451afce4cc7f4afa39b5afdcb4
spz377 <48015574+spz377@users.noreply.github.com>
2019-05-21 16:22:25 +0200

    Add files via upload

---
+++ SU19-Exercises/SpaceTaxi-1/Customer.cs
@@ -0,0 +1,40 @@
+using System.Drawing;
+using System.IO;
+using System.Xml;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+
+namespace SpaceTaxi_1 {
+    public class Customer {
+
+        private string name;
+        private int spawntime;
+        private char spawnplatform;
+        private char landplatform;
+        private int droptime;
+        private int droppoints;
+        private readonly DIKUArcade.Graphics.Image image1;
+        private DynamicShape shape;
+        private Entity entity;
+        
+        
+        public Customer(string name, int spawntime, char spawnplatform, char landplatform, int droptime, int droppoints) {
+            this.name = name;
+            this.spawntime = spawntime;
+            this.spawnplatform = spawnplatform;
+            this.landplatform = landplatform;
+            this.droptime = droptime;
+            this.droppoints = droppoints;
+            
+            shape = new DynamicShape(new Vec2F(), new Vec2F());
+            image1 = new DIKUArcade.Graphics.Image(Path.Combine("Assets","Images", "CustomerStandLeft.png"));
+            entity = new Entity(shape, image1);
+
+        }
+        public void RenderCustomer() {
+            entity.RenderEntity();
+            
+        }
+    } 
+}
